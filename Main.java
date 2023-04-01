643b708eca613ae6d9a7eba924ad7296e5c0510d
Himanshu Sharma <83121258+Himnshu-Shrma@users.noreply.github.com>
2023-04-01 21:01:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Taking input
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();//Next takes only one word
+        String line=sc.nextLine();
+        //nextInt() will take integer
+
+        System.out.println();
+
+    }
+}
